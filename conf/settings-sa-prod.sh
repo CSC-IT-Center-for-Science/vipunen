66bf1197a58bddded4840c31a575b3d669719985
Lauri Jokipii <lauri.jokipii@csc.fi>
2016-11-10 20:45:24 +0200

    Make settings more generic. Add setting for Opintopolku

--- conf/settings-sa-prod.sh
+++ conf/settings-sa-prod.sh
@@ -1,2 +1,3 @@
 export DATABASE_HOST="dwipvipusql16.csc.fi:1433"
 export DATABASE_NAME="VIPUNEN_SA"
+export OPINTOPOLKU=virkailija.opintopolku.fi
--- conf/settings-sa-test.sh
+++ conf/settings-sa-test.sh
@@ -1,2 +1,3 @@
 export DATABASE_HOST="dwitvipusql16.csc.fi:1433"
 export DATABASE_NAME="VIPUNEN_SA"
+export OPINTOPOLKU=testi.virkailija.opintopolku.fi
