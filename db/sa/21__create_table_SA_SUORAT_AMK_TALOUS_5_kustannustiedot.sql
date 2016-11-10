f81bf09aa0de264d527ce0bb30191077ca89db73
Lauri Jokipii <lauri.jokipii@csc.fi>
2016-11-10 16:31:30 +0200

    Make SQL scripts consistent. Remove elsewhere difficulf "GO". Make all files UTF-8. Clear scripts 1 and 2 as those operations have been decided to be done by admins of the system.

--- db/sa/21__create_table_SA_SUORAT_AMK_TALOUS_5_kustannustiedot.sql
+++ db/sa/21__create_table_SA_SUORAT_AMK_TALOUS_5_kustannustiedot.sql
@@ -1,33 +1,25 @@
-USE [VIPUNEN_SA]
-GO
-/****** Object:  Table [dbo].[SA_SUORAT_AMK_TALOUS_5_kustannustiedot]    Script Date: 10.11.2016 11:16:37 ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
-IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[SA_SUORAT_AMK_TALOUS_5_kustannustiedot]') AND type in (N'U'))
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'dbo.SA_SUORAT_AMK_TALOUS_5_kustannustiedot') AND type in (N'U'))
 BEGIN
-CREATE TABLE [dbo].[SA_SUORAT_AMK_TALOUS_5_kustannustiedot](
-	[AMK] [nvarchar](50) NULL,
-	[TILIK] [int] NULL,
-	[KOULALA] [nvarchar](50) NULL,
-	[AMK_koodi] [nvarchar](50) NULL,
-	[OHJALA] [int] NULL,
-	[VTKUSTY] [bigint] NULL,
-	[KOULUTUS] [bigint] NULL,
-	[TUTKOUL] [bigint] NULL,
-	[PTUTOSAT] [bigint] NULL,
-	[AMMOPKOUL] [bigint] NULL,
-	[MUUKOUL] [bigint] NULL,
-	[TUTKIMUS] [bigint] NULL,
-	[YLAMKKOUL] [bigint] NULL,
-	[TKTOIM] [bigint] NULL,
-	[MUUYHTEI] [bigint] NULL,
-	[CHECKSUM_AMK_TALOUS_5_KUSTANNUSTIEDOT] [int] NULL,
-	[IMP_CREATED_DATE] [datetime] NULL,
-	[IMP_UPDATED_DATE] [datetime] NULL,
-	[IMP_CREATED_BY] [nvarchar](255) NULL,
-	[IMP_DELETED_DATE] [datetime] NULL
-) ON [PRIMARY]
+CREATE TABLE dbo.SA_SUORAT_AMK_TALOUS_5_kustannustiedot(
+	AMK nvarchar(50) NULL,
+	TILIK int NULL,
+	KOULALA nvarchar(50) NULL,
+	AMK_koodi nvarchar(50) NULL,
+	OHJALA int NULL,
+	VTKUSTY bigint NULL,
+	KOULUTUS bigint NULL,
+	TUTKOUL bigint NULL,
+	PTUTOSAT bigint NULL,
+	AMMOPKOUL bigint NULL,
+	MUUKOUL bigint NULL,
+	TUTKIMUS bigint NULL,
+	YLAMKKOUL bigint NULL,
+	TKTOIM bigint NULL,
+	MUUYHTEI bigint NULL,
+	CHECKSUM_AMK_TALOUS_5_KUSTANNUSTIEDOT int NULL,
+	IMP_CREATED_DATE datetime NULL,
+	IMP_UPDATED_DATE datetime NULL,
+	IMP_CREATED_BY nvarchar(255) NULL,
+	IMP_DELETED_DATE datetime NULL
+)
 END
-GO
--- db/sa/2__add_user.sql
+++ db/sa/2__add_user.sql
@@ -1,18 +1,2 @@
--- nb! login tulee olla jo!
--- tässä lisätään siis user loginille, jolla tätä skriptiä luultavasti ajetaan, joten ei ihan mahdollinen tilanne
-
--- ajo onnistuu luonnollisesti vain tunnuksella jolla on oikeus luoda käyttäjiä
--- kierretään asia nyt sillä, että koko roska on tämän olemassaolo-iffin sisällä
-
-IF NOT EXISTS (select * from sys.user_token where name='t_sql16_etl') BEGIN
-
-CREATE USER [t_sql16_etl] FOR LOGIN [t_sql16_etl] WITH DEFAULT_SCHEMA=[dbo]
-;--GO
-ALTER ROLE [db_ddladmin] ADD MEMBER [t_sql16_etl]
-;--GO
-ALTER ROLE [db_datareader] ADD MEMBER [t_sql16_etl]
-;--GO
-ALTER ROLE [db_datawriter] ADD MEMBER [t_sql16_etl]
-;--GO
-
-END
+-- Poistettu skriptin sisältö.
+-- Päädyttiin siihen, että ajotunnus ja oikeuksien jakaminen tulee tehdä ensin vahvemmin tunnuksin ihmistyönä.
