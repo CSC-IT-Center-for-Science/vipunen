dbf32b8bd26f229072390809089b79b416a9ed5e
Lauri Jokipii <lauri.jokipii@csc.fi>
2016-11-19 10:16:59 +0200

    Immediate fix (didn't check in time, sorry)

--- db/sa/53__alter_table_sa_virta_jtp_julkaisut_add_columns_pvm.sql
+++ db/sa/53__alter_table_sa_virta_jtp_julkaisut_add_columns_pvm.sql
@@ -3,7 +3,7 @@ IF NOT EXISTS (
   where TABLE_SCHEMA='dbo' and TABLE_NAME='sa_virta_jtp_julkaisut'
   and COLUMN_NAME='muutospvm'
 ) BEGIN
-ALTER TABLE dbo.sa_virta_jtp_julkaisut ADD COLUMN muutospvm	varchar(50) null
+ALTER TABLE dbo.sa_virta_jtp_julkaisut ADD muutospvm varchar(50) null
 END
 
 IF NOT EXISTS (
@@ -11,5 +11,5 @@ IF NOT EXISTS (
   where TABLE_SCHEMA='dbo' and TABLE_NAME='sa_virta_jtp_julkaisut'
   and COLUMN_NAME='luontipvm'
 ) BEGIN
-ALTER TABLE dbo.sa_virta_jtp_julkaisut ADD COLUMN luontipvm	varchar(50) null
+ALTER TABLE dbo.sa_virta_jtp_julkaisut ADD luontipvm varchar(50) null
 END
