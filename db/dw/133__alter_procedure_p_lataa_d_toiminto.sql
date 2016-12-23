20b2fc1f17080eb3371cb1c40d716f9c64e46b48
Jouko Rouhiainen <jouko.rouhiainen@csc.fi>
2016-12-23 13:05:05 +0200

    AMK Talous: added key values

--- db/dw/133__alter_procedure_p_lataa_d_toiminto.sql
+++ db/dw/133__alter_procedure_p_lataa_d_toiminto.sql
@@ -10,5 +10,9 @@ VALUES
 (2, N'Muu yhteiskunnallinen toiminta', N'Muu yhteiskunnallinen toiminta', N'Muu yhteiskunnallinen toiminta', N'Muu yhteiskunnallinen toiminta', CAST(N'2016-11-10T08:00:33.513' AS DateTime), N'p_lataa_d_toiminto', N'DWI\jrouhiai'),
 (3, N'Toiminnot yhteensä', N'Toiminnot yhteensä', N'Toiminnot yhteensä', N'Toiminnot yhteensä', CAST(N'2016-11-10T08:00:33.513' AS DateTime), N'p_lataa_d_toiminto', N'DWI\jrouhiai'),
 (4, N'Tutkimus- ja kehitystoiminta', N'Tutkimus- ja kehitystoiminta', N'Tutkimus- ja kehitystoiminta', N'Tutkimus- ja kehitystoiminta', CAST(N'2016-11-10T08:00:33.517' AS DateTime), N'p_lataa_d_toiminto', N'DWI\jrouhiai'),
-(5, N'Yhteiskulut', N'Yhteiskulut', N'Yhteiskulut', N'Yhteiskulut', CAST(N'2016-11-10T08:00:33.517' AS DateTime), N'p_lataa_d_toiminto', N'DWI\jrouhiai')
+(5, N'Yhteiskulut', N'Yhteiskulut', N'Yhteiskulut', N'Yhteiskulut', CAST(N'2016-11-10T08:00:33.517' AS DateTime), N'p_lataa_d_toiminto', N'DWI\jrouhiai'),
+(6, N'Tutkimustoiminta', N'Tutkimustoiminta', N'Tutkimustoiminta', N'Tutkimustoiminta', CAST(N'2016-11-10T08:00:33.517' AS DateTime), N'p_lataa_d_toiminto', N'DWI\jrouhiai'),
+(7, N'Taiteellinen toiminta', N'Taiteellinen toiminta', N'Taiteellinen toiminta', N'Taiteellinen toiminta', CAST(N'2016-11-10T08:00:33.517' AS DateTime), N'p_lataa_d_toiminto', N'DWI\jrouhiai'),
+(8, N'Harjoittelukoulu', N'Harjoittelukoulu', N'Harjoittelukoulu', N'Harjoittelukoulu', CAST(N'2016-11-10T08:00:33.517' AS DateTime), N'p_lataa_d_toiminto', N'DWI\jrouhiai'),
+(9, N'Kansalliskirjasto', N'Kansalliskirjasto', N'Kansalliskirjasto', N'Kansalliskirjasto', CAST(N'2016-11-10T08:00:33.517' AS DateTime), N'p_lataa_d_toiminto', N'DWI\jrouhiai')
 SET IDENTITY_INSERT dbo.d_toiminto OFF
